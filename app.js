13e27142516b124d45cc1522afad46a1ebe8aeaa
Gabriel de Oliveira Santos <gabrieloliveira0094@gmail.com>
2025-08-10 22:58:48 -0300

    First Steps

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
+let arrayDeAmigos = [];
+
+function adicionarAmigo () {
+    let amigo = document.querySelector ('input').value;
+
+    if (amigo == "") {
+        alert("Por favor, insira um nome válido.");
+    } else {
+        arrayDeAmigos.push(amigo);
+        console.log(arrayDeAmigos);
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    amigo = document.querySelector('input');
+    amigo.value = "";
+}
